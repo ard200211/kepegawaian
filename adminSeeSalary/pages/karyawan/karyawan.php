a7f20674642210d3163c63951310568d41b67bca
Hafiz <feermanss18@gmail.com>
2022-08-06 10:06:15 +0700

    Bug Fixing #2

--- adminSeeSalary/pages/karyawan/karyawan.php
+++ adminSeeSalary/pages/karyawan/karyawan.php
@@ -111,7 +111,7 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $_SESSION['username']?></span>
                                 <img class="img-profile rounded-circle"
                                     src="../../dist/img/ic_admin.png">
                             </a>
--- adminSeeSalary/pages/karyawan/tambahKaryawan.php
+++ adminSeeSalary/pages/karyawan/tambahKaryawan.php
@@ -10,4 +10,71 @@
         ";
     }
 
+
+
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    
+
+<div class="container">
+    <div class="mt-5 mb-5">
+        <h1>
+            <b>Insert Data Karyawan</b>
+        </h1>
+
+        <div class="mt-5">
+            <table class="table">
+                <form action="" method="post">
+                    <tr>
+                        <th>NIK</th>
+                        <td><input type="text" name="nik" value="<?= random_int(10, 100000)?>" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <th>Nama</th>
+                        <td><input type="text" name="nama" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <th>Jenis Kelamin</th>
+                        <td>
+                            <select name="jk">
+                                <option value="Laki-laki">Laki-laki</option>
+                                <option value="Perempuan">Perempuan</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Telepon</th>
+                        <td><input type="text" name="telepon" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <th>Alamat</th>
+                        <td><input type="text" name="alamat" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <th>Password</th>
+                        <td>input</td>
+                    </tr>
+                </form>
+            </table>
+        </div>
+
+    </div>
+</div>
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
